a36da03e021b1c6acf3a65f69157df0f61842bf1
sydneyB6577 <sydneyburt04@gmail.com>
2025-04-02 20:03:47 -0600

    Add SnakeHandout files

---
+++ PS9/SnakeHandout/GUI/GUI.Client/Pages/SnakeGUI.razor.js
@@ -0,0 +1,14 @@
+﻿
+window.initRenderJS = (instance) => {
+    window.theInstance = instance;
+};
+
+document.addEventListener('keydown', function (event)
+{
+    // Optionally log the key for testing
+    console.log('Key pressed:', event.key);
+
+    // Call the C# method and pass the key pressed
+    theInstance.invokeMethodAsync('HandleKeyPress', event.key);
+
+});
