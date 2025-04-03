d3e6ed19ca44741942656dd1449576cb58135c8d
Sydney Burt <u1406577@umail.utah.edu>
2025-04-02 20:07:08 -0600

    About page

--- PS9/SnakeHandout/GUI/GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
+++ PS9/SnakeHandout/GUI/GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec744e51be1f9b0ad565a07e125cf7c03d618935")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
