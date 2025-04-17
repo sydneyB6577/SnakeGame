9eadfc145401579d05e28778dd0bc636c8e4f3f9
LeviH501 <hammondLG456@gmail.com>
2025-04-17 11:23:57 -0600

    Created some setup for the WebServer portion of PS10.

--- ps8_handout/WebServer/Program.cs
+++ ps8_handout/WebServer/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using CS3500.Networking;
+using Microsoft.AspNetCore.Hosting.Server;
+using MySqlX.XDevAPI;
+
+namespace WebServer
+{
+    public static class WebServer
+    {
+        public static void Main(string[] args)
+        {
+            // start the server with the HandleHttpConnection deligate pass
+            
+            Console.Read();
+        }
+
+        private static void HandleHttpConnection(NetworkConnection connection)
+        {
+            Console.WriteLine("new client");
+            string request = connection.ReadLine();
+            Console.WriteLine(request);
+
+            if(request.Contains("GET / "))
+            {
+                // serve to the display page
+            }
+            else if(request.Contains("GET /games"))
+            {
+                // serve to the table of all the games in the database
+            }
+            else if(request.Contains("GET /games?gid=x"))
+            {
+                // serve to the stats for a specific game of a given gameID "x"
+            }
+            else
+            {
+                // if the serve is not found
+            }
+        }
+    }
+}
+
+//< html >
+//  < h3 > Welcome to the Snake Games Database!</h3>
+//  <a href="/games">View Games</a>
+//</html>
