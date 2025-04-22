503a2bba07698a0e5a839a85a1906bea43f9f878
LeviH501 <hammondLG456@gmail.com>
2025-04-22 14:06:13 -0600

    We can generate the htlm home page. All we have to do is fix the other two pages and make friendly exceptions.

--- ps8_handout/WebServer/Program.cs
+++ ps8_handout/WebServer/Program.cs
@@ -16,7 +16,8 @@ namespace WebServer
         private const string httpOkHeader = "HTTP/1.1 200 OK\r\n" +
             "Connection: close\r\n" +
             "Content-Type: text/html; charset=UTF-8\r\n" +
-            "Content-Length: 89";
+            "Content-Length: 89\r\n" + 
+            "\r\n";
 
         private const string httpBadHeader = "HTTP/1.1 404 Not Found\r\n" +
             "Connection: close\r\n" +
@@ -34,7 +35,8 @@ namespace WebServer
 
         /// <summary>
         /// 
-        /// </summary>
+        /// </summary>+		connection	{GUI.Client.Controllers.NetworkConnection}	GUI.Client.Controllers.NetworkConnection
+
         /// <param name="connection"> Server fills the database. </param>
         private static void HandleHttpConnection(NetworkConnection connection)
         {
@@ -46,15 +48,17 @@ namespace WebServer
             {
                 // serve to the display page
                 string response = httpOkHeader;
-                response += "<html>\r\n" + "<h3>Welcome to the Snake Games Database!</h3>\r\n" + "<a href=\"/games\">View Games</a>\r\n</html>"; // might have to separate into multiple response += strings in case this is wrong. COME BACK!!!
+                response += "<html>\r\n" + "<h3>Welcome to the Snake Games Database!</h3>\r\n" + "<a href=\"/games\">View Games</a>\r\n" + "</html>"; // might have to separate into multiple response += strings in case this is wrong. COME BACK!!!
+                Console.WriteLine(response);
                 connection.Send(response);
+                Console.WriteLine(response);
             }
             else if(request.Contains("GET /games"))
             {
                 // serve to the table of all the games in the database)
                 string response = httpOkHeader;
 
-                response += "<html>" + "<table border=\"1\">" + "<thead>" + "<tr>" + "<td>ID</td><td>Start</td><td>End</td>" + "</tr>" + "</thead>" + "<tbody>";
+                response += "<html>\r\n" + "<table border=\"1\">\r\n" + "<thead>\r\n" + "<tr>\r\n" + "<td>ID</td><td>Start</td><td>End</td>\r\n" + "</tr>\r\n" + "</thead>\r\n" + "<tbody>\r\n";
 
                 using (MySqlConnection connectionOne = new MySqlConnection(NetworkController.connectDatabaseString))
                 {
@@ -66,27 +70,28 @@ namespace WebServer
                     {
                         while (reader.Read())
                         {
-                            response += "<tr>";
-                            response += "<td>" + "<a href=\"/games?gid=" + reader["GameID"] + ">" + reader["GameID"] + "</a>" +  "</td>";
-                            response += "<td>" + reader["StartTime"] + "</td>";
-                            response += "<td>" + reader["EndTime"] + "</td>";
-                            response += "</tr>";
+                            response += "<tr>\r\n";
+                            response += "<td>" + "<a href=\"/games?gid=" + reader["GameID"] + ">" + reader["GameID"] + "</a>" + "</td>\r\n";
+                            response += "<td>" + reader["StartTime"] + "</td>\r\n";
+                            response += "<td>" + reader["EndTime"] + "</td>\r\n";
+                            response += "</tr>\r\n";
                         }
                     }
                 }
-                response += "</tbody>" + "</table>" + "</html>";
+                response += "</tbody>\r\n" + "</table>\r\n" + "</html>\r\n";
                 connection.Send(response);
                 //Disconnects from snake server even when server # is 80.
                 //connection.Disconnect();
             }
-            else if(request.Contains("GET /games?gid=x"))
+            else if(request.Contains("GET /games?gid="))
             {
+
                 // serve to the stats for a specific game of a given gameID "x"
                 string response = httpOkHeader;
 
-                response += "<html>" + "<h3>" + "Stats for Game X" + "</h3>" + "<table border=\"1\">" +
-                    "<thead>" + "<tr>" + "<td>Player ID</td><td>Player Name</td><td>Max Score</td><td>Enter Time</td><td>Leave Time</td>" +
-                    "</tr>" + "<tbody>";
+                response += "<html>\r\n" + "<h3>\r\n" + "Stats for Game " + "1" + "</h3>\r\n" + "<table border=\"1\">\r\n" +
+                    "<thead>\r\n" + "<tr>\r\n" + "<td>Player ID</td><td>Player Name</td><td>Max Score</td><td>Enter Time</td><td>Leave Time</td>\r\n" +
+                    "</tr>\r\n" + "<tbody>\r\n";
                 using (MySqlConnection connectionTwo = new MySqlConnection(NetworkController.connectDatabaseString))
                 {
                     connectionTwo.Open();
@@ -97,16 +102,16 @@ namespace WebServer
                         while (reader.Read())
                         {
                             response += "<tr>";
-                            response += "<td>" + reader["PlayerID"] + "</td>";
-                            response += "<td>" + reader["PlayerName"] + "</td>";
-                            response += "<td>" + reader["MaxScore"] + "</td>";
-                            response += "<td>" + reader["EnterTime"] + "</td>";
-                            response += "<td>" + reader["LeaveTime"] + "</td>";
-                            response += "</tr>";
+                            response += "<td>" + reader["PlayerID"] + "</td>\r\n";
+                            response += "<td>" + reader["PlayerName"] + "</td>\r\n";
+                            response += "<td>" + reader["MaxScore"] + "</td>\r\n";
+                            response += "<td>" + reader["EnterTime"] + "</td>\r\n";
+                            response += "<td>" + reader["LeaveTime"] + "</td>\r\n";
+                            response += "</tr>\r\n";
                         }
                     }
                 }
-                response += "</tbody>" + "</table>" + "</html>";
+                response += "</tbody>\r\n" + "</table>\r\n" + "</html>\r\n";
                 connection.Send(response);
             }
             else
